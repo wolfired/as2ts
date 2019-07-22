4b6632fca15579f16c5120a73f73d7cb60e6bbd3
LinkWu <wjianl@qq.com>
2019-07-22 14:56:28 +0800

    增加注释

--- src/flash/utils/ByteArray.ts
+++ src/flash/utils/ByteArray.ts
@@ -1,21 +1,23 @@
 import { Endian } from "./Endian"
 
-enum Mode {
-    Read = 1,
-    Write = 2,
-}
+const POINTER_POSITION: number = 0x1;
+const POINTER_LENGTH: number = 0x2;
+const POINTER_CAPACITY: number = 0x4;
 
-const BYTES_1: number = 1;
-const BYTES_2: number = 2;
-const BYTES_4: number = 4;
-const BYTES_8: number = 8;
+const BYTES_1: number = 0x1;
+const BYTES_2: number = 0x2;
+const BYTES_4: number = 0x4;
+const BYTES_8: number = 0x8;
 
 const utf8encoder: TextEncoder = new TextEncoder();
 const utf8decoder: TextDecoder = new TextDecoder();
 
+/**
+ * 提供用于优化读取/写入以及处理二进制数据的方法和属性.
+ */
 export class ByteArray {
-    public constructor(raw?: DataView) {
-        this._raw = void 0 === raw ? new DataView(new ArrayBuffer(0)) : raw;
+    public constructor() {
+        this._raw = new DataView(new ArrayBuffer(0));
         this.endian = Endian.BIG_ENDIAN;
         this.position = 0;
         this.length = 0;
@@ -31,27 +33,27 @@ export class ByteArray {
         return this._endian ? Endian.LITTLE_ENDIAN : Endian.BIG_ENDIAN;
     }
 
-    public set endian(value: Endian) {
-        this._endian = Endian.LITTLE_ENDIAN === value;
+    public set endian(endian: Endian) {
+        this._endian = Endian.LITTLE_ENDIAN === endian;
     }
 
     public get length(): number {
         return this._length;
     }
 
-    public set length(value: number) {
-        if (this._length === value) {
+    public set length(newLen: number) {
+        if (this._length === newLen) {
             return;
         }
 
-        if (this._length < value) {
-            this.need(value - this._length, Mode.Write);
+        if (this._length < newLen) {
+            this.checkCapacity(newLen);
         }
 
-        this._length = value;
+        this._length = newLen;
 
         if (this._position > this._length) {
-            this._position = this._length;
+            this.position = this._length;
         }
     }
 
@@ -67,239 +69,347 @@ export class ByteArray {
         this._position = value;
     }
 
+    /**
+     * ByteArray 的剩余可读取字节数
+     */
     public get bytesAvailable(): number {
-        return Math.max(0, this._length - this._position);
+        if (this._length > this._position) {
+            return this._length - this._position;
+        }
+
+        return 0;
     }
 
+    /**
+     * 清空 ByteArray 的数据
+     */
     public clear(): void {
         this.position = 0;
         this.length = 0;
     }
 
+    /**
+     * 从字节流中读取布尔值.
+     * 
+     * 读取单个字节, 如果字节非零, 则返回 true, 否则返回 false.
+     */
     public readBoolean(): boolean {
-        this.need(BYTES_1, Mode.Read);
+        this.checkLength(BYTES_1);
 
         let value = this._raw.getUint8(this._position);
 
-        this.move(BYTES_1, Mode.Read);
+        this.movePointer(BYTES_1, POINTER_POSITION);
 
-        return 0 !== value
+        return 0 !== value;
     }
 
+    /**
+     * 从字节流中读取带符号的字节.
+     * 
+     * 返回值的范围是从 -128 到 127.
+     */
     public readByte(): number {
-        this.need(BYTES_1, Mode.Read);
+        this.checkLength(BYTES_1);
 
         let value = this._raw.getInt8(this._position);
 
-        this.move(BYTES_1, Mode.Read);
+        this.movePointer(BYTES_1, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取一个 IEEE 754 双精度(64 位)浮点数.
+     */
     public readDouble(): number {
-        this.need(BYTES_8, Mode.Read);
+        this.checkLength(BYTES_8);
 
         let value = this._raw.getFloat64(this._position, this._endian);
 
-        this.move(BYTES_8, Mode.Read);
+        this.movePointer(BYTES_8, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取一个 IEEE 754 单精度(32 位)浮点数.
+     */
     public readFloat(): number {
-        this.need(BYTES_4, Mode.Read);
+        this.checkLength(BYTES_4);
 
         let value = this._raw.getFloat32(this._position, this._endian);
 
-        this.move(BYTES_4, Mode.Read);
+        this.movePointer(BYTES_4, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取一个带符号的 32 位整数.
+     * 
+     * 返回值的范围是从 -2147483648 到 2147483647.
+     */
     public readInt(): number {
-        this.need(BYTES_4, Mode.Read);
+        this.checkLength(BYTES_4);
 
         let value = this._raw.getInt32(this._position, this._endian);
 
-        this.move(BYTES_4, Mode.Read);
+        this.movePointer(BYTES_4, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取一个带符号的 16 位整数.
+     * 
+     * 返回值的范围是从 -32768 到 32767.
+     */
     public readShort(): number {
-        this.need(BYTES_2, Mode.Read);
+        this.checkLength(BYTES_2);
 
         let value = this._raw.getInt16(this._position, this._endian);
 
-        this.move(BYTES_2, Mode.Read);
+        this.movePointer(BYTES_2, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取无符号的字节.
+     * 
+     * 返回值的范围是从 0 到 255.
+     */
     public readUnsignedByte(): number {
-        this.need(BYTES_1, Mode.Read);
+        this.checkLength(BYTES_1);
 
         let value = this._raw.getUint8(this._position);
 
-        this.move(BYTES_1, Mode.Read);
+        this.movePointer(BYTES_1, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取一个无符号的 32 位整数.
+     * 
+     * 返回值的范围是从 0 到 4294967295.
+     */
     public readUnsignedInt(): number {
-        this.need(BYTES_4, Mode.Read);
+        this.checkLength(BYTES_4);
 
         let value = this._raw.getUint32(this._position, this._endian);
 
-        this.move(BYTES_4, Mode.Read);
+        this.movePointer(BYTES_4, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取一个无符号的 16 位整数.
+     * 
+     * 返回值的范围是从 0 到 65535.
+     */
     public readUnsignedShort(): number {
-        this.need(BYTES_2, Mode.Read);
+        this.checkLength(BYTES_2);
 
         let value = this._raw.getUint16(this._position, this._endian);
 
-        this.move(BYTES_2, Mode.Read);
+        this.movePointer(BYTES_2, POINTER_POSITION);
 
         return value;
     }
 
+    /**
+     * 从字节流中读取一个 UTF-8 字符串. 假定字符串的前缀是无符号的短整型(以字节表示长度).
+     */
     public readUTF(): string {
         return this.readUTFBytes(this.readUnsignedShort());
     }
 
+    /**
+     * 从字节流中读取一个由 length 参数指定的 UTF-8 字节序列, 并返回一个字符串.
+     * 
+     * @param length 
+     */
     public readUTFBytes(length: number): string {
-        this.need(length, Mode.Read);
+        this.checkLength(length);
 
         let str: string = utf8decoder.decode(new Uint8Array(this._raw.buffer, this._position, length));
 
-        this.move(length, Mode.Read);
+        this.movePointer(length, POINTER_POSITION);
 
         return str;
     }
 
+    /**
+     * 写入布尔值. 根据 value 参数写入单个字节. 如果为 true, 则写入 1, 如果为 false, 则写入 0.
+     * 
+     * @param value 
+     */
     public writeBoolean(value: boolean): void {
-        this.need(BYTES_1, Mode.Write);
+        this.checkCapacity(this._position + BYTES_1);
 
         this._raw.setUint8(this._position, value ? 1 : 0);
 
-        this.move(BYTES_1, Mode.Write);
+        this.movePointer(BYTES_1, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 在字节流中写入一个字节.
+     * 
+     * 使用参数的低 8 位. 忽略高 24 位.
+     * 
+     * @param value 
+     */
     public writeByte(value: number): void {
-        this.need(BYTES_1, Mode.Write);
+        this.checkCapacity(this._position + BYTES_1);
 
         this._raw.setInt8(this._position, value);
 
-        this.move(BYTES_1, Mode.Write);
+        this.movePointer(BYTES_1, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 在字节流中写入一个 IEEE 754 双精度（64 位）浮点数.
+     * 
+     * @param value 
+     */
     public writeDouble(value: number): void {
-        this.need(BYTES_8, Mode.Write);
+        this.checkCapacity(this._position + BYTES_8);
 
         this._raw.setFloat64(this._position, value, this._endian);
 
-        this.move(BYTES_8, Mode.Write);
+        this.movePointer(BYTES_8, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 在字节流中写入一个 IEEE 754 单精度(32 位)浮点数.
+     * 
+     * @param value 
+     */
     public writeFloat(value: number): void {
-        this.need(BYTES_4, Mode.Write);
+        this.checkCapacity(this._position + BYTES_4);
 
         this._raw.setFloat32(this._position, value, this._endian);
 
-        this.move(BYTES_4, Mode.Write);
+        this.movePointer(BYTES_4, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 在字节流中写入一个带符号的 32 位整数.
+     * 
+     * @param value 
+     */
     public writeInt(value: number): void {
-        this.need(BYTES_4, Mode.Write);
+        this.checkCapacity(this._position + BYTES_4);
 
         this._raw.setInt32(this._position, value, this._endian);
 
-        this.move(BYTES_4, Mode.Write);
+        this.movePointer(BYTES_4, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 在字节流中写入一个 16 位整数. 使用参数的低 16 位. 忽略高 16 位.
+     * 
+     * @param value 
+     */
     public writeShort(value: number): void {
-        this.need(BYTES_2, Mode.Write);
+        this.checkCapacity(this._position + BYTES_2);
 
         this._raw.setInt16(this._position, value, this._endian);
 
-        this.move(BYTES_2, Mode.Write);
+        this.movePointer(BYTES_2, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 在字节流中写入一个无符号的 32 位整数.
+     * 
+     * @param value 
+     */
     public writeUnsignedInt(value: number): void {
-        this.need(BYTES_4, Mode.Write);
+        this.checkCapacity(this._position + BYTES_4);
 
         this._raw.setUint32(this._position, value, this._endian);
 
-        this.move(BYTES_4, Mode.Write);
+        this.movePointer(BYTES_4, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 将 UTF-8 字符串写入字节流. 先写入以字节表示的 UTF-8 字符串长度(作为 16 位整数), 然后写入表示字符串字符的字节.
+     * 
+     * @param value 
+     */
     public writeUTF(value: string): void {
         let bs = utf8encoder.encode(value);
 
         this.writeShort(bs.length);
 
-        this.need(bs.length, Mode.Write);
+        this.checkCapacity(this._position + bs.length);
 
-        this.byte2byte(bs, new Uint8Array(this._raw.buffer, this._position));
+        copy(new Uint8Array(this._raw.buffer, this._position), bs);
 
-        this.move(bs.length, Mode.Write);
+        this.movePointer(bs.length, POINTER_POSITION | POINTER_LENGTH);
     }
 
+    /**
+     * 将 UTF-8 字符串写入字节流. 类似于 writeUTF() 方法，但 writeUTFBytes() 不使用 16 位长度的词为字符串添加前缀.
+     * 
+     * @param value 
+     */
     public writeUTFBytes(value: string): void {
         let bs = utf8encoder.encode(value);
 
-        this.need(bs.length, Mode.Write);
+        this.checkCapacity(this._position + bs.length);
 
-        this.byte2byte(bs, new Uint8Array(this._raw.buffer, this._position));
+        copy(new Uint8Array(this._raw.buffer, this._position), bs);
 
-        this.move(bs.length, Mode.Write);
+        this.movePointer(bs.length, POINTER_POSITION | POINTER_LENGTH);
     }
 
-    private need(bytes: number, mode: Mode = Mode.Write): void {
-        if (Mode.Read == mode) {
-            if (this.bytesAvailable < bytes) {
-                throw "EOFError";
-            }
-            return;
+    private checkLength(needBytes: number): void {
+        if (this.bytesAvailable < needBytes) {
+            throw "EOFError";
         }
 
-        let capOld = this._raw.byteLength;
+        return;
+    }
 
-        if (0 === capOld) {
-            this._raw = new DataView(new ArrayBuffer(bytes))
+    private checkCapacity(newCap: number): void {
+        let oldCap = this._raw.byteLength;
+
+        if (0 === oldCap) {
+            this._raw = new DataView(new ArrayBuffer(newCap));
         } else {
-            let capNew = this._position + bytes
-
-            while (capOld < capNew) {
-                capOld += capOld
+            while (oldCap < newCap) {
+                oldCap += oldCap;
             }
 
-            this.byte2byte(new Uint8Array(this._raw.buffer), new Uint8Array((this._raw = new DataView(new ArrayBuffer(capOld))).buffer));
+            let src = new Uint8Array(this._raw.buffer, 0, this._length);
+            let dst = new Uint8Array((this._raw = new DataView(new ArrayBuffer(oldCap))).buffer);
+            
+            copy(dst, src);
         }
     }
 
-    private move(bytes: number, mode: Mode = Mode.Write): void {
-        this._position += bytes;
+    private movePointer(moveBytes: number, pointerType: number): void {
+        let newPos = this._position + moveBytes;
 
-        if (Mode.Read == mode) {
-            return;
+        if (0 !== (POINTER_POSITION & pointerType)) {
+            this._position = newPos;
         }
 
-        if (this._length < this._position) {
-            this._length = this._position;
+        if (0 !== (POINTER_LENGTH & pointerType) && this._length < newPos) {
+            this._length = newPos;
         }
     }
+}
 
-    private byte2byte(src: Uint8Array, dst: Uint8Array): number {
-        let length: number = Math.min(src.length, dst.length);
+function copy(dst: Uint8Array, src: Uint8Array): number {
+    let length: number = Math.min(dst.byteLength, src.byteLength);
 
-        for (let i = 0; i < length; ++i) {
-            dst[i] = src[i];
-        }
-
-        return length;
+    for (let i = 0; i < length; ++i) {
+        dst[i] = src[i];
     }
+
+    return length;
 }
--- test/flash/utils/ByteArray_test.ts
+++ test/flash/utils/ByteArray_test.ts
@@ -1,58 +1,78 @@
 import { ByteArray } from "../../../src/flash/utils/ByteArray";
 import { expect } from "chai";
 import "mocha";
+import { Endian } from "../../../src/flash/utils/Endian";
 
-describe("ByteArray class", () => {
-    it("make a ByteArray", () => {
+describe("test ByteArray class", () => {
+    it("test new", () => {
         let ba = new ByteArray();
+
+        expect(ba.endian).to.equal(Endian.BIG_ENDIAN);
         expect(ba.position).to.equal(0);
         expect(ba.length).to.equal(0);
-        expect(ba.bytesAvailable).to.equal(0);
+    });
 
-        ba.length = 2;
-        expect(ba.position).to.equal(0);
-        expect(ba.length).to.equal(2);
+    it("test set length", () => {
+        let ba = new ByteArray();
+
+        ba.length = 4;
+
+        expect(ba.length).to.equal(4);
+
+        ba.length = 8;
+
+        expect(ba.length).to.equal(8);
+
+        ba.length = 13;
+
+        expect(ba.length).to.equal(13);
+
+        ba.length = 16;
+
+        expect(ba.length).to.equal(16);
+    });
+
+    it("test bytesAvailable", () => {
+        let ba = new ByteArray();
+
+        ba.length = 4;
+
+        expect(ba.bytesAvailable).to.equal(4);
+
+        ba.position = 2
+
         expect(ba.bytesAvailable).to.equal(2);
 
         ba.position = 4;
-        expect(ba.position).to.equal(4);
-        expect(ba.length).to.equal(2);
-        expect(ba.bytesAvailable).to.equal(0);
 
-        ba.length = 9;
-        expect(ba.position).to.equal(4);
-        expect(ba.length).to.equal(9);
-        expect(ba.bytesAvailable).to.equal(5);
+        expect(ba.bytesAvailable).to.equal(0);
     });
 
-    it("write and read bool", () => {
+    it("test clear", () => {
         let ba = new ByteArray();
 
-        ba.length = 1;
-        expect(ba.length).to.equal(1);
-        expect(ba.readBoolean()).to.equal(false);
+        ba.length = 4;
+        ba.position = 4;
 
-        ba.position = 3;
+        ba.clear();
+
+        expect(ba.length).to.equal(0);
+
+        expect(ba.position).to.equal(0);
+    });
+
+    it("test write and read bool", () => {
+        let ba = new ByteArray();
 
         ba.writeBoolean(true);
-        ba.writeBoolean(false);
-        ba.writeByte(0);
-        ba.writeByte(1);
+
         ba.position = 0;
 
-        expect(ba.readBoolean()).to.equal(false);
-        expect(ba.readBoolean()).to.equal(false);
-        expect(ba.readBoolean()).to.equal(false);
         expect(ba.readBoolean()).to.equal(true);
-        expect(ba.readBoolean()).to.equal(false);
-        expect(ba.readBoolean()).to.equal(false);
-        expect(ba.readBoolean()).to.equal(true);
-
-        // expect(() => ba.readBoolean()).to.throw("EOFError");
     });
 
     it("write and read i8/i16/i32", () => {
-        let ba = new ByteArray(new DataView(new ArrayBuffer(0)));
+        let ba = new ByteArray();
         ba.writeByte(-1);
         ba.writeShort(-1);
         ba.writeInt(-1);
@@ -64,7 +84,7 @@ describe("ByteArray class", () => {
     });
 
     it("write and read u8/u16/u32", () => {
-        let ba = new ByteArray(new DataView(new ArrayBuffer(0)));
+        let ba = new ByteArray();
         ba.writeByte(1);
         ba.writeShort(1);
         ba.writeUnsignedInt(1);
@@ -76,7 +96,7 @@ describe("ByteArray class", () => {
     });
 
     it("write and read f32/f64", () => {
-        let ba = new ByteArray(new DataView(new ArrayBuffer(0)));
+        let ba = new ByteArray();
         ba.writeDouble(3.14);
         ba.writeDouble(9.8);
         ba.position = 0;
@@ -86,7 +106,7 @@ describe("ByteArray class", () => {
     });
 
     it("write and read string", () => {
-        let ba = new ByteArray(new DataView(new ArrayBuffer(0)));
+        let ba = new ByteArray();
         ba.writeUTF("你好，我叫DayDayUp。");
         ba.writeUTF("你好，我是新来的犀利哥。");
         ba.position = 0;
